880744a3a7ae80306bed0e361b2572bec1c6b458
plebsicle <praval555pro@gmail.com>
2025-05-26 22:51:17 +0530

    turbo repo and express server setup

---
+++ apps/server/src/cors-config/cors.ts
@@ -0,0 +1,16 @@
+const allowedOrigins : string[] = ['http://localhost:3000'];
+
+const corsConfiguration = {
+    origin : (origin : string | undefined ,callback : (err: Error | null, allow?: boolean) => void ) : void =>{
+        if(!origin) return callback(null,true);
+        if(allowedOrigins.includes(origin)){
+            return callback(null,true);
+        }
+        else{
+            return callback(new Error(`CORS not allowed from this origin`));
+        }
+    },
+    credentials : true
+}
+
+export default corsConfiguration;
---
+++ apps/server/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { Express } from 'express';
+import cors from 'cors'
+import helmet from 'helmet';
+import corsConfiguration from './cors-config/cors';
+
+
+// Standard Contansts for express application
+const app : Express = express();
+const PORT : number = parseInt(process.env.PORT!) || 8000;
+
+
+// Standard Starter Middlewares
+app.use(cors(corsConfiguration));
+app.use(helmet());
+app.use(express.json());
+
+//Routes
+
+
+
+//Security Measure
+app.disable('x-powered-by');
+
+
+// Server Started
+app.listen(PORT,'0.0.0.0',()=>{
+    console.log(`Server Started on PORT ${PORT}`)
+});
+
